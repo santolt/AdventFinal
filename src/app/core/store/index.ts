13b42056ed117c68877dd91fdd9e817df7773ff0
Santiago Cardona <ssuarez@lean-tech.io>
2022-08-18 00:18:08 -0500

    store ngrx

--- src/app/core/store/index.ts
+++ src/app/core/store/index.ts
@@ -1,4 +1,6 @@
 import {ActionReducerMap} from "@ngrx/store"
+// import {AppState} from "@app-models/app.model";
+
 // import {appState} from "@app-core/store/models/app.model";
 // import {filterReducer} from "@app-core/"
 
